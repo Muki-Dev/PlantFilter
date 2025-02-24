8621b71dedd9a6d9f546fd19fdef4f953e3b8865
muki Dev <mukidev881@gmail.com>
2025-02-24 11:57:30 +0000

    add PlantList Style

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,11 +1,11 @@
-
+import '../styles/PlantList.css'
 const PlantList = ({ plants, selectedCategory }) => {
 	const filteredPlants = selectedCategory 
 		? plants.filter((plant) => plant.category === selectedCategory)
 		: plants;
 
 		return(
-			<div>
+			<div className='plant-list'>
 				<h3>Liste des plantes</h3>
 				<ul>
 				{filteredPlants.map((plant) => (
